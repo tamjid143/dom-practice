8d1da39385f88b5928ce3f8cabebfe1d1c793ae3
Tamjid <tamjid143@pop-os.localdomain>
2022-08-15 23:21:27 +0600

    hello from linux

--- dom-1st/script.js
+++ dom-1st/script.js
@@ -1,1 +1,7 @@
-console.log("Hello From DOM");
+const domHeader = (document.getElementById("dom-header").style.color =
+  "purple");
+console.log(domHeader);
+const lovingThings = (document.getElementsByClassName(
+  "loving-things li"
+).style.color = "purple");
+console.log(lovingThings);
